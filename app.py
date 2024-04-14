f8f53c90482851d104a188ef32b5a1c44415fa85
EthanChu <154274616+EthanChu890515@users.noreply.github.com>
2024-04-14 19:41:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -83,3 +83,6 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+@app.route("/")
+def index():
+    return "Hello, World! This is my Line Bot."
