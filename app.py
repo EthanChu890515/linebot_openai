b8a43e9b504c1d88f3e97c92f6b3e9c677f6f8f1
EthanChu <154274616+EthanChu890515@users.noreply.github.com>
2024-10-12 02:49:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -246,6 +246,11 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text='歡迎使用MBTI機器人！如果要開始測驗，請輸入"開始"。')
         )
+          if user_message == "兇手是作家":
+        reply_message = "答案正確，恭喜破關！"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
+        return
+
 
 def send_question_with_buttons(reply_token, question):
     question_parts = question.split("\na) ")
